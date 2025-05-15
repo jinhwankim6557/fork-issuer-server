6f2986ccedbcea00fe819e9bbe8340af876fe16b
gw-nam <gwnam@raoncorp.com>
2025-05-15 18:46:56 +0900

    chore: modify Sample Data (#30)
    - remove pkey
    - modify TAS Url

--- source/did-issuer-server/src/main/resources/db/changelog/data/insert_namespace.sql
+++ source/did-issuer-server/src/main/resources/db/changelog/data/insert_namespace.sql
@@ -1,14 +1,12 @@
 INSERT INTO namespace (
-    id, namespace_id, name, ref, schema_claims
+    namespace_id, name, ref, schema_claims
 ) VALUES (
-             1,
              'org.iso.18013.5',
              'ISO/IEC 18013-5:2021 - Personal identification',
              'https://www.iso.org/standard/69084.html',
              '{"namespace":{"id":"org.iso.18013.5","name":"ISO/IEC 18013-5:2021 - Personal identification","ref":"https://www.iso.org/standard/69084.html"},"items":[{"id":"family_name","caption":"Family Name","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null},{"id":"given_name","caption":"Given Name","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null},{"id":"birth_date","caption":"Birth date","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null},{"id":"address","caption":"Address","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null},{"id":"document_number","caption":"Document Number","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null},{"id":"issue_date","caption":"Issue Date","type":"text","format":"plain","hideValue":false,"location":null,"required":null,"description":null,"i18n":null}]}'
          ),
       (
-           2,
            'org.opendid.v1.national_id',
            'OpenDID National ID',
            'https://opendid.org/schema/v1/claim',
